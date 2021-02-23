d2c42c9d46187f5b928b55883413d91d1dfa81e3
Bryan Killian <bryan@trailmix.me>
2021-02-23 15:13:15 -0500

    add explicit types

--- _deps.ts
+++ _deps.ts
@@ -2,6 +2,9 @@ export type {
   PageProps,
   PagicLayout,
 } from "https://deno.land/x/pagic@v1.2.0/mod.ts";
+// @deno-types="https://cdn.pagic.org/@types/react@16.9.50/index.d.ts"
+// @deno-types="https://cdn.pagic.org/@types/react-dom@16.9.8/index.d.ts"
+// @deno-types="https://cdn.pagic.org/@types/react-dom@16.9.8/server/index.d.ts"
 export {
   React,
   ReactDOM,
